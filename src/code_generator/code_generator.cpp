9cd108a8b85c86906dd6146fa136c2a2aca3c9f6
Ali Yahya <alive@ali01.com>
2010-08-10 21:26:33 -0700

    added code_generator module

---
+++ src/code_generator/code_generator.cpp
@@ -0,0 +1,306 @@
+#include "code_generator.h"
+
+/* project includes */
+#include <ast.h>
+
+CodeGenerator::CodeGenerator(Simone::Ptr<Program> _prog) {
+  node_functor_ = NodeFunctor::NodeFunctorNew();
+  _prog->apply(node_functor_);
+}
+
+/* top level */
+void
+CodeGenerator::NodeFunctor::operator()(Program *nd) {
+  Decl::Ptr decl;
+  Program::const_decl_iter it = nd->declsBegin();
+  for (; it != nd->declsEnd(); ++it) {
+    decl = *it;
+    process_node(decl);
+  }
+}
+
+
+/* decl */
+void
+CodeGenerator::NodeFunctor::operator()(Decl *nd) {
+  Identifier::Ptr id = nd->identifier();
+  process_node(id);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(VarDecl *nd) {
+  /* apply this functor to upcasted nd */
+  (*this)(static_cast<Decl*>(nd));
+
+  Type::Ptr type = nd->type();
+  process_node(type);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(FnDecl *nd) {
+  /* apply this functor to upcasted nd */
+  (*this)(static_cast<Decl*>(nd));
+
+  Type::Ptr return_type = nd->returnType();
+  process_node(return_type);
+
+  VarDecl::Ptr decl;
+  FnDecl::const_formal_iter it = nd->formalsBegin();
+  for (; it != nd->formalsEnd(); ++it) {
+    decl = *it;
+    process_node(decl);
+  }
+
+  /* stmt_block could be NULL in the case of a function prototype */
+  StmtBlock::Ptr stmt_block = nd->body();
+  process_node(stmt_block);
+}
+
+
+/* decl/object */
+void
+CodeGenerator::NodeFunctor::operator()(ObjectDecl *nd) {
+  /* apply this functor to upcasted nd */
+  (*this)(static_cast<Decl*>(nd));
+
+  Decl::Ptr decl;
+  ClassDecl::const_member_iter it = nd->membersBegin();
+  for (; it != nd->membersEnd(); ++it) {
+    decl = *it;
+    process_node(decl);
+  }
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ClassDecl *nd) {
+  /* apply this functor to upcasted nd */
+  (*this)(static_cast<ObjectDecl*>(nd));
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(InterfaceDecl *nd) {
+  /* apply this functor to upcasted nd */
+  (*this)(static_cast<ObjectDecl*>(nd));
+}
+
+
+/* stmt */
+void
+CodeGenerator::NodeFunctor::operator()(StmtBlock *nd) {
+  VarDecl::Ptr var_decl;
+  StmtBlock::const_decl_iter decl_it = nd->declsBegin();
+  for (; decl_it != nd->declsEnd(); ++decl_it) {
+    var_decl = *decl_it;
+    process_node(var_decl);
+  }
+
+  Stmt::Ptr stmt;
+  StmtBlock::const_stmt_iter stmt_it = nd->stmtsBegin();
+  for (; stmt_it != nd->stmtsEnd(); ++stmt_it) {
+    stmt = *stmt_it;
+    process_node(stmt);
+  }
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(PrintStmt *nd) {
+  Expr::Ptr arg;
+  PrintStmt::const_arg_iterator it = nd->argsBegin();
+  for (int i = 1; it != nd->argsEnd(); ++it, ++i) {
+    arg = *it;
+    process_node(arg);
+  }
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ReturnStmt *nd) {
+  Expr::Ptr expr = nd->expr();
+  process_node(expr);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(BreakStmt *nd) {
+  
+}
+
+
+/* stmt/conditional */
+void
+CodeGenerator::NodeFunctor::operator()(ConditionalStmt *nd) {
+  Expr::Ptr test = nd->test();
+  process_node(test);
+
+  Stmt::Ptr body = nd->body();
+  process_node(body);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(IfStmt *nd) {
+  /* applying this functor on upcasted nd */
+  (*this)(static_cast<ConditionalStmt*>(nd));
+
+  Stmt::Ptr else_body = nd->elseBody();
+  process_node(else_body);
+}
+
+
+/* stmt/conditional/loop */
+void
+CodeGenerator::NodeFunctor::operator()(ForStmt *nd) {
+  Expr::Ptr init = nd->init();
+  process_node(init);
+
+  /* applying this functor on upcasted nd */
+  (*this)(static_cast<ConditionalStmt*>(nd));
+
+  Expr::Ptr step = nd->step();
+  process_node(step);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(WhileStmt *nd) {
+  /* applying this functor on upcasted nd */
+  (*this)(static_cast<ConditionalStmt*>(nd));
+}
+
+
+/* stmt/expr */
+void
+CodeGenerator::NodeFunctor::operator()(AssignExpr *nd) {
+  LValueExpr::Ptr l_val = nd->left();
+  process_node(l_val);
+
+  Operator::Ptr op = nd->op();
+  process_node(op);
+
+  Expr::Ptr rhs = nd->right();
+  process_node(rhs);
+}
+
+
+/* stmt/expr/call_expr */
+void
+CodeGenerator::NodeFunctor::operator()(CallExpr *nd) {
+  Identifier::Ptr function = nd->function();
+  process_node(function);
+
+  Expr::Ptr actual;
+  FunctionCallExpr::const_actuals_iter it = nd->actualsBegin();
+  for (; it != nd->actualsEnd(); ++it) {
+    actual = *it;
+    process_node(actual);
+  }
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(FunctionCallExpr *nd) {
+  /* applying this functor on upcasted nd */
+  (*this)(static_cast<CallExpr*>(nd));
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(MethodCallExpr *nd) {
+  Expr::Ptr base = nd->base();
+  process_node(base);
+
+  /* applying this functor on upcasted nd */
+  (*this)(static_cast<CallExpr*>(nd));
+}
+
+
+/* stmt/expr/single_addr */
+void
+CodeGenerator::NodeFunctor::operator()(NewExpr *nd) {
+  
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(NewArrayExpr *nd) {
+  Expr::Ptr size = nd->size();
+  process_node(size);
+
+  ArrayType::Ptr type = nd->arrayType();
+  process_node(type);
+}
+
+
+/* stmt/expr/single_addr/compound */
+void
+CodeGenerator::NodeFunctor::operator()(CompoundExpr *nd) {
+  Expr::Ptr left = nd->left();
+  process_node(left);
+
+  Operator::Ptr op = nd->op();
+  process_node(op);
+
+  Expr::Ptr right = nd->right();
+  process_node(right);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ArithmeticExpr *nd) {
+  (*this)(static_cast<CompoundExpr*>(nd));
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(LogicalExpr *nd) {
+  (*this)(static_cast<CompoundExpr*>(nd));
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(RelationalExpr *nd) {
+  (*this)(static_cast<CompoundExpr*>(nd));
+}
+
+
+/* stmt/expr/single_addr/l_value */
+void
+CodeGenerator::NodeFunctor::operator()(VarAccessExpr *nd) {
+  Identifier::Ptr id = nd->identifier();
+  process_node(id);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ArrayAccessExpr *nd) {
+  Expr::Ptr base = nd->base();
+  process_node(base);
+
+  Expr::Ptr subscript = nd->subscript();
+  process_node(subscript);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(FieldAccessExpr *nd) {
+  Expr::Ptr base = nd->base();
+  process_node(base);
+
+  Identifier::Ptr id = nd->field();
+  process_node(id);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ThisExpr *nd) {
+  
+}
+
+
+/* type */
+void
+CodeGenerator::NodeFunctor::operator()(NamedType *nd) {
+  Identifier::Ptr type_id = nd->identifier();
+  process_node(type_id);
+}
+
+void
+CodeGenerator::NodeFunctor::operator()(ArrayType *nd) {
+  Type::Ptr type = nd->elemType();
+  process_node(type);
+}
+
+/* CodeGenerator private member functions */
+
+void
+CodeGenerator::NodeFunctor::process_node(Node::Ptr _node) {
+  if (_node)
+    _node->apply(this);
+}
---
+++ src/code_generator/code_generator.h
@@ -0,0 +1,124 @@
+#ifndef CODE_GENERATOR_H_TBNBRHRW
+#define CODE_GENERATOR_H_TBNBRHRW
+
+/* simone includes */
+#include <simone/ptr_interface.h>
+
+/* ast includes */
+#include <ast/node.h>
+
+/* forward declarations */
+class Program;
+
+class CodeGenerator : public Simone::PtrInterface<CodeGenerator> {
+public:
+  typedef Simone::Ptr<const CodeGenerator> PtrConst;
+  typedef Simone::Ptr<CodeGenerator> Ptr;
+
+  static Ptr CodeGeneratorNew(Simone::Ptr<Program> _program) {
+    return new CodeGenerator(_program);
+  }
+
+private:
+  CodeGenerator(Simone::Ptr<Program> _program);
+
+  /* implementation of code generator using double dispatch mechanism */
+  class NodeFunctor : public Node::Functor {
+    public:
+      typedef Simone::Ptr<const NodeFunctor> PtrConst;
+      typedef Simone::Ptr<NodeFunctor> Ptr;
+
+      static Ptr NodeFunctorNew() {
+        return new NodeFunctor();
+      }
+
+      /* top level */
+      /* void operator()(Node *); */
+      void operator()(Program *);
+      /* void operator()(Identifier *); */
+      /* void operator()(Operator *); */
+
+      /* decl */
+      void operator()(Decl *);
+      void operator()(VarDecl *);
+      void operator()(FnDecl *);
+
+      /* decl/object */
+      void operator()(ObjectDecl *);
+      void operator()(ClassDecl *);
+      void operator()(InterfaceDecl *);
+
+      /* stmt */
+      /* void operator()(Stmt *); */
+      void operator()(StmtBlock *);
+      void operator()(PrintStmt *);
+      void operator()(ReturnStmt *);
+      void operator()(BreakStmt *);
+
+      /* stmt/conditional */
+      void operator()(ConditionalStmt *);
+      void operator()(IfStmt *);
+      
+      /* stmt/conditional/loop */
+      /* void operator()(LoopStmt *); */
+      void operator()(ForStmt *);
+      void operator()(WhileStmt *);
+
+      /* stmt/expr */
+      /* void operator()(Expr *); */
+      void operator()(AssignExpr *);
+      /* void operator()(NullExpr *); */
+      /* void operator()(EmptyExpr *); */
+
+      /* stmt/expr/call_expr */
+      void operator()(CallExpr *);
+      void operator()(FunctionCallExpr *);
+      void operator()(MethodCallExpr *);
+
+      /* stmt/expr/single_addr */
+      /* void operator()(SingleAddrExpr *); */
+      /* void operator()(BoolConstExpr *); */
+      /* void operator()(IntConstExpr *); */
+      /* void operator()(DblConstExpr *); */
+      /* void operator()(StrConstExpr *); */
+      /* void operator()(ReadLineExpr *); */
+      /* void operator()(ReadIntegerExpr *); */
+      void operator()(NewExpr *);
+      void operator()(NewArrayExpr *);
+
+      /* stmt/expr/single_addr/compound */
+      void operator()(CompoundExpr *);
+      void operator()(ArithmeticExpr *);
+      void operator()(LogicalExpr *);
+      void operator()(RelationalExpr *);
+
+      /* stmt/expr/single_addr/l_value */
+      /* void operator()(LValueExpr *); */
+      void operator()(VarAccessExpr *);
+      void operator()(ArrayAccessExpr *);
+      void operator()(FieldAccessExpr *);
+      void operator()(ThisExpr *);
+
+      /* type */
+      /* void operator()(Type *); */
+      void operator()(NamedType *);
+      void operator()(ArrayType *);
+
+    private:
+      NodeFunctor() {}
+
+      /* member functions */
+      void process_node(Node::Ptr _nd);
+
+      
+  };
+
+  /* data members */
+  NodeFunctor::Ptr node_functor_;
+
+  /* disallowed operations */
+  CodeGenerator(const CodeGenerator&);
+  void operator=(const CodeGenerator&);
+};
+
+#endif
